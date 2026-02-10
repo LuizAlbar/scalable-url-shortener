facda8b9ed80de957b1593d4a15dc1fdf8e16bef
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-10 12:33:25 -0300

    feat: add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { UrlShortenerModule } from "./modules/url-shortener/url-shortener.module";
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		UrlShortenerModule,
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
