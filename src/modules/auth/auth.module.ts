cd69aea3022e5f5e0c4c9c2cc9f3bdf29132624f
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-27 23:46:50 -0300

    feat: add auth module

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module, type OnModuleInit } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { env } from "src/config/env";
+import { AuthController } from "./controllers/auth.controller";
+import { AccountModel } from "./models/auth.model";
+import { AuthService } from "./services/auth.service";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+
+@Module({
+	imports: [
+		JwtModule.register({
+			secret: env.JWT_SECRET,
+			signOptions: { expiresIn: "15m" },
+		}),
+	],
+	controllers: [AuthController],
+	providers: [AuthService, JwtStrategy],
+})
+export class AuthModule implements OnModuleInit {
+	async onModuleInit() {
+		AccountModel.syncDB((err: any, result: any) => {
+			if (err) throw err;
+			console.log("Schema synchronized: ", result);
+		});
+	}
+}
