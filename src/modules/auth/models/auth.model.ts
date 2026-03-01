b9b307763911811bc9e4bdfb35e015fba03d8cea
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-28 22:03:48 -0300

    fix: set account email as sec index on schema

--- src/modules/auth/models/auth.model.ts
+++ src/modules/auth/models/auth.model.ts
@@ -4,16 +4,17 @@ export const AccountModel = models.loadSchema("Account", {
 	fields: {
 		id: { type: "text", key: "partitioned" },
 		email: { type: "text" },
-		passoword: { type: "text" },
+		password: { type: "text" },
 		created_at: {
 			type: "timestamp",
 			default: { $db_function: "toTimestamp(now())" },
 		},
 	},
 	key: ["id"],
+	indexes: ["email"],
 	options: {
 		table_name: "accounts",
 		model: "Account",
-		timestamp: { created_at: "created_at" },
+		timestamps: { created_at: "created_at" },
 	},
 });
