102655da03ba5e0af0ae0877e4e01e84d801b5cf
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-28 22:55:34 -0300

    feat: add swagger, cookies & plug auth module on nest app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./modules/auth/auth.module";
 import { UrlShortenerModule } from "./modules/url-shortener/url-shortener.module";
 
 @Module({
 	imports: [
 		UrlShortenerModule,
+		AuthModule,
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import fastifyCookie from "@fastify/cookie";
 import { NestFactory } from "@nestjs/core";
 import {
 	FastifyAdapter,
 	type NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { env } from "./config/env";
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,7 +14,23 @@ async function bootstrap() {
 		new FastifyAdapter(),
 	);
 	app.setGlobalPrefix("api/v1");
+
+	await app.register(fastifyCookie, {
+		secret: env.JWT_SECRET,
+	});
+
+	const config = new DocumentBuilder()
+		.setTitle("Scalable URL Shortener")
+		.setDescription("API documentation")
+		.setVersion("1.0")
+		.addBearerAuth()
+		.addCookieAuth("access_token")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api/docs", app, document);
 	await app.listen(3000, "0.0.0.0");
+	console.log(`Swagger docs at: http://localhost:3000/api/docs`);
 }
 
 bootstrap();
