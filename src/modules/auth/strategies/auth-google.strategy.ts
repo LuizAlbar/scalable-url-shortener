1c285252ece558f4a1b7977f48229d4c3c32f29c
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-03-01 12:16:31 -0300

    feat: add oauth20 google stategy

---
+++ src/modules/auth/strategies/auth-google.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, VerifyCallback } from "passport-google-oauth20";
+import { env } from "src/config/env";
+
+@Injectable()
+export class AuthGoogleStrategy extends PassportStrategy(Strategy, "google") {
+	constructor() {
+		super({
+			clientID: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://localhost:3000/api/v1/auth/google/callback",
+			scope: ["email", "profile"],
+		});
+	}
+
+	async validate(
+		accessToken: string,
+		refreshToken: string,
+		profile: any,
+		done: VerifyCallback,
+	): Promise<any> {
+		const { name, emails } = profile;
+		const user = {
+			email: emails[0].value,
+			firstName: name.givenName,
+			lastName: name.familyName,
+			accessToken,
+		};
+		done(null, user);
+	}
+}
