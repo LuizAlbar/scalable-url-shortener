6c7c7d3fe949ef9b94c804683d7aa5f0eb94e873
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-03-01 14:37:59 -0300

    feat: add forgot password & reset password functions

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { hashPasswordService } from "src/common/hash-password-service";
+import { MailService } from "src/common/mail-service";
 import { models } from "src/config/database/cassandra/cassandra.client";
 import { uuidv7 } from "uuidv7";
 import { AuthJwtDTO } from "../dtos/auth-dto";
@@ -8,7 +9,10 @@ import { AuthJwtDTO } from "../dtos/auth-dto";
 @Injectable()
 export class AuthService {
 	private readonly authModel = models.instance.Account;
-	constructor(private jwtService: JwtService) {}
+	constructor(
+		private jwtService: JwtService,
+		private mailService: MailService,
+	) {}
 
 	async register(email: string, password: string) {
 		const hashedPassword = await hashPasswordService.hashPassword(password);
@@ -77,5 +81,58 @@ export class AuthService {
 				access_token: await this.jwtService.signAsync(payload),
 			};
 		} catch {}
+	}
+
+	async forgotPassword(email: string) {
+		const account = await this.authModel.findOneAsync({
+			email: email,
+		});
+
+		if (!account) {
+			throw Error("Account not found");
+		}
+
+		if (account.reset_token || account.reset_token_expires) {
+			account.reset_token = null;
+			account.reset_token_expires = null;
+		}
+
+		const token = uuidv7();
+		const expires = new Date();
+		expires.setHours(expires.getMinutes() + 15);
+
+		account.reset_token = token;
+		account.reset_token_expires = expires;
+
+		await account.saveAsync();
+
+		await this.mailService.sendResetPasswordEmail(email, token);
+
+		return {
+			message:
+				"If an account with that email exists, a reset link has been sent.",
+		};
+	}
+
+	async resetPassword(email: string, token: string, newPassword: string) {
+		const account = await this.authModel.findOneAsync({
+			email: email,
+		});
+
+		if (!account || account.reset_token_expires < new Date()) {
+			throw new Error("Invalid or expired token");
+		}
+
+		if (account.reset_token !== token) {
+			throw new Error("Invalid token");
+		}
+
+		account.password = await hashPasswordService.hashPassword(newPassword);
+		account.reset_token = null;
+		account.reset_token_expires = null;
+
+		await account.saveAsync();
+
+		return { message: "Password has been reset successfully" };
 	}
 }
