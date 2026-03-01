2088d671afda733a8d010456dad0315432fb5593
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-02-28 22:51:30 -0300

    feat: add jwt auth guard on url routes

--- src/modules/url-shortener/controllers/url-shortener.controller.ts
+++ src/modules/url-shortener/controllers/url-shortener.controller.ts
@@ -6,7 +6,9 @@ import {
 	Param,
 	Post,
 	Redirect,
+	UseGuards,
 } from "@nestjs/common";
+import { JwtAuthGuard } from "src/modules/auth/guards/jwt-auth.guard";
 import { CreateUrlDTO } from "../dtos/create-url.dto";
 import { UrlShortenerService } from "../services/url-shortener.service";
 
@@ -14,12 +16,14 @@ import { UrlShortenerService } from "../services/url-shortener.service";
 export class UrlController {
 	constructor(private readonly urlService: UrlShortenerService) {}
 
+	@UseGuards(JwtAuthGuard)
 	@Post()
 	@HttpCode(201)
 	async shortenUrl(@Body() createUrlDto: CreateUrlDTO) {
 		return this.urlService.shortenUrl(createUrlDto.longUrl);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(":shortId")
 	@Redirect()
 	async getLongUrl(@Param("shortId") shortId: string) {
